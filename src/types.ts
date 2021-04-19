f694f1b5be40ca01099fc8caa44ea567767ba8ab
THWinther <43626565+THWinther@users.noreply.github.com>
2021-04-19 20:18:36 +0000

    Byrja á interface tpyes

--- src/types.ts
+++ src/types.ts
@@ -6,5 +6,28 @@ export interface ICharacter {
   // TODO fleiri týpur
 }
 
+export interface ICharacterConnection {
+  [index : number]: {
+    characters : ICharacter;
+  }
+}
+
+export interface IFilm {
+  episodeID: number;
+  title: string;
+  openingCrawl : string;
+  characterConnection : ICharacterConnection;
+}
+
+export interface IFilms {
+  [index : number]: {
+    films : IFilm;
+  }
+}
+
+export interface IAllFilms {
+  films : IFilms;
+}
+
 // TODO hér ættum við að útbúa interface fyrir öll gögn sem við vinnum með (t.d. IFilm, IPaging)
 // og svör sem við fáum frá GraphQL endapunkti
